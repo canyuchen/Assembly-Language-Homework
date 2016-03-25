8e4a44b5787b6c655140105193d525100aa8446f
aldehydecho <aldehydecho@hotmail.com>
2016-03-25 19:30:33 +0800

    每两位翻转一个字符

---
+++ week 2/r.s
@@ -0,0 +1,28 @@
+.section .data
+stringvar:
+	.ascii "0123456789abcdef"
+.section .text
+.globl _start
+_start:
+
+#reverse
+	movl $8,%ecx
+	movl $stringvar,%eax
+r:
+    movb (%eax),%bl
+    xchgb 1(%eax),%bl
+    mov %bl,(%eax)
+	addl $2,%eax
+	loop r
+
+#output
+	movl $4,%eax
+	movl $1,%ebx
+	movl $stringvar,%ecx
+	movl $16,%edx
+	int $0x80
+
+#exit
+	movl $1,%eax
+	movl $0,%ebx
+	int $0x80
